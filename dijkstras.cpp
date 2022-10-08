cdfafa08ea4f6e2e9c92fdc758bf391f979770d4
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2022-10-08 17:11:49 +0530

    Update dijkstras.cpp

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -1,1 +1,95 @@
 #include<iostream>
+#include<string.h>
+#include<climits>
+using namespace std;
+#define INF 999
+int mindistance(int distance[],bool stat[])
+{
+	int minimum=INT_MAX,ind;
+	for(int k=0;k<30;k++)
+	{
+		if(stat[k]==false && distance[k]<=minimum)
+		{
+			minimum=distance[k];
+			ind=k;
+		}
+	}
+	return ind;
+}
+void dijkstra(int g[30][30],int source,int destination, string stations[30])
+{
+	int distance[56];
+	bool stat[56];
+	for(int k=0;k<56;k++)
+	{
+		distance[k]=INT_MAX;
+		stat[k]=false;
+	}
+	distance[source]=0;
+	for(int k=0;k<30;k++)
+	{
+		int m=mindistance(distance,stat);
+		stat[m]=true;
+		for(int k=0;k<30;k++)
+		{
+			if(!stat[k] && g[m][k] && distance[m]!=INT_MAX && distance[m]+g[m][k]<distance[k])
+				distance[k]=distance[m]+g[m][k];
+		}
+	}
+	for(int k=0;k<30;k++)
+	{
+		if(k==destination)
+		cout<<"Minimum Stations from "<<stations[source]<<" To "<<stations[destination]<<"  are  "<<distance[k]<<endl;
+	}
+}
+
+int main()  {
+
+int g[30][30]= {
+ {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0},
+ {0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0},
+ {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0},
+ {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1},
+ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0}} ;
+
+ int source;
+ int destination;
+ string stations[30]={"AMBABARI","PANIPECH","SUBHASH NAGAR","SINDHI CAMP","GOVERNMENT HOSTEL","AJMERI GATE","SMS HOSPITAL","NARAYAN SINGH CIRCLE","SAWAI MANSINGH STADIUM","GANDHI NAGAR MODE","TONY PHATAK","DEV NAGAR","GOPAL PURA","MAHAVIR NAGAR","DURGAPURA","LAXMI NAGAR","SANGANER","HALDI GHATI GATE","PRATAP NAGAR","SITAPUR INDUSTRIAL AREA","BADI CHOPAR","CHOTI CHOPAR","CHANDPOLE","RAILWAY STATION","CIVIL LINES","RAM NAGAR","SHYAM NAGAR","VIVEK NAGAR","NEW AATISH MARKET","MANSAROVAR"};
+
+ cout<<"Enter the source station"<<endl;
+ cout<<"Enter the destination station"<<endl;
+ for(int i=0 ; i<30 ; i++)  {
+    cout<<"Enter "<< i <<" for "<<stations[i]<<endl;
+ }
+ cin>>source;
+ cin>>destination;
+ dijkstra(g,source,destination,stations);
+
+    return 0;
+}
+
